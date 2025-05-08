b3d0cb0907d946402c0e7c7baafa7611b327b198
Dominik Lorenc <dominiklorenc1270@gmail.com>
2025-05-08 21:56:57 +0200

    Update App component title to reflect version 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">
-          🚀 Rick and Morty - Fan Service Beta Version{' '}
+          🚀 Rick and Morty - Fan Service Beta Version 1.0.0
         </h1>
       </div>
       <Outlet />
